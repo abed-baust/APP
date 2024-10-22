7748210ccc770cea36e608980ead3f4a65e16164
Abedur Rahman <abed.baust@gmail.com>
2024-10-22 08:07:00 +0600

    [Added] [IGenericRepository added update]

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604ab63e1a6b34e72deb0034b0b916f8d9a8956e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+231e3f7c028cdb908047e9d9fa985dce7d48f69e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604ab63e1a6b34e72deb0034b0b916f8d9a8956e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+231e3f7c028cdb908047e9d9fa985dce7d48f69e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
